c85ceddc74128eb44c5425f8130eab369c63505b
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-04 22:44:04 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,52 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info('Hello logs!', { structuredData: true });
-	response.send('Hello from Firebase!');
+const app = express();
+
+// GET all posts
+app.get('/posts', (req, res, next) => {
+	admin
+		.firestore()
+		.collection('posts')
+		.orderBy('createdAt', 'desc')
+		.get()
+		.then(querySnapshot => {
+			const posts = [];
+			querySnapshot.forEach(doc => posts.push({ id: doc.id, ...doc.data() }));
+			res.json(posts);
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({
+				error: 'unable to get posts',
+			});
+		});
 });
+
+// create a post
+app.post('/post', (req, res, next) => {
+	const post = {
+		...req.body,
+		createdAt: admin.firestore.Timestamp.now(),
+	};
+	admin
+		.firestore()
+		.collection('posts')
+		.add(post)
+		.then(doc => {
+			res.json({
+				message: `document ${doc.id} created successfully`,
+			});
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({
+				error: 'creating post failed',
+			});
+		});
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
