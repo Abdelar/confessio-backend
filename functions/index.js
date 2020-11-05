d7033f42e2d9ab9bfee14ba1e98ab64cf3ec5eb3
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-05 22:14:18 +0100

    Add cors

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+import cors from 'cors';
 
 admin.initializeApp();
 
 const app = express();
+
+app.use(cors());
 
 const db = admin.firestore();
 
